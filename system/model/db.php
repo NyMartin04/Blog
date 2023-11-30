d8235fa5887b5b83705324fdb1843e5ca41d046c
martinkovacs22 <79226892+martinkovacs22@users.noreply.github.com>
2023-11-30 19:23:34 +0100

    DBExceptionThrow

--- system/model/db.php
+++ system/model/db.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace model;
-        
+
+use config\Exception;
+
     class Db{
         public static function connectToDatabase($dbName, $dbUser, $dbPass, $dbHost = '127.0.0.1'){
             $dsn = "mysql:host=".$dbHost.";dbname=".$dbName.";charset=utf8mb4";
@@ -27,40 +29,10 @@ namespace model;
             return array("err" => false, "data" => $resultSet);
 
             } catch (\Throwable $th) {
-                return array("err" => true, "data" => $th->getMessage());
+                return Exception::msg(array("err"=>true,"data"=>$th->getMessage()));
             }
         }
 
-        public static function executeQuery($sql, $params = null){
-            self::$error = null;
-
-            $query = self::$db->prepare($sql);
-            $success = $query->execute($params);
-
-            if(!$success){
-                $error = $query->errorInfo();
-                self::$error = $error[2];
-            }
-            return $query;
-        }
-        
-        public static function Query($sql, $params = null, $className = null){
-            $query = self::executeQuery($sql, $params);
-
-            if($query){
-                if(!$className){
-                    $result = $query->fetchAll(\PDO::FETCH_ASSOC);
-                } else{
-                    $result = $query->fetchAll(\PDO::FETCH_CLASS, $className);
-                }
-                return $result;
-            }
-            return null;
-        }
-        public static function err(){
-            return self::$error;
-        }
 
         private static $db;
-        private static $error;
     }
