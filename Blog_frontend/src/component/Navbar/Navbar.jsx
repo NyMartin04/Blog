4c025c779f0d4950bd87b3d8f7303101ffa23a54
martinkovacs22 <79226892+martinkovacs22@users.noreply.github.com>
2023-12-27 13:22:58 +0100

    searchData

--- Blog_frontend/src/component/Navbar/Navbar.jsx
+++ Blog_frontend/src/component/Navbar/Navbar.jsx
@@ -11,9 +11,16 @@ import baseFun from '../../config/baseFun.js';
 const Navbar = () => {
 
     const [isLogin, setIsLogin] = useState(false);
-    const [searchData, setSearchData] = useState(Array);
-    const [searchValue, setSearchValue] = useState(false);
+    const [searchValue, setSearchValue] = useState([]);
+    const [ImageData, setImageData] = useState(null);
+    const [fileId, setFileId] = useState([]);
+
+    const handlerFileId = (data) => {
+        // Az új elemet hozzáadja a fileId tömbhöz
+        setFileId(prevFileId => [...prevFileId, data]);
+    }
     
+
     useEffect(() => {
         if (Cookie.getCookie("token")) {
             // fetch.postData("verify", {}).then(data => {
@@ -23,76 +30,117 @@ const Navbar = () => {
             //         Cookie.setCookie("token", data.JWT, { expires: 1800 })
             //     }
             // })
-            dataHandler.postDataAndHandle("verify",{})
-            .then(res=>{
-                if (!res.err) {
-                    setIsLogin(!res.err) 
-                baseFun.login(res.JWT)
-                
-                }else{
-                    baseFun.logout()
-                }
-                
-            })
-            .catch(err=>{
-                console.error(err);
-            })
+            dataHandler.postDataAndHandle("verify", {})
+                .then(res => {
+                    if (!res.err) {
+                        setIsLogin(!res.err)
+                        baseFun.login(res.JWT)
+
+                    } else {
+                        baseFun.logout()
+                    }
+
+                })
+                .catch(err => {
+                    console.error(err);
+                })
         }
     }, [Cookie.getCookie("token")])
+    useEffect(() => {
+        const fetchData = async () => {
 
-    const searchUser =(e)=>{
+            await dataHandler.postDataAndHandle('getFile', { fileId: 1 }).then(res => {
+
+                console.log(res.data);
+                const byteCharacters = atob(res.data);
+                const byteNumbers = new Array(byteCharacters.length);
+
+                for (let i = 0; i < byteCharacters.length; i++) {
+                    byteNumbers[i] = byteCharacters.charCodeAt(i);
+                }
+
+                const byteArray = new Uint8Array(byteNumbers);
+
+                // Készítünk egy Blob objektumot a byte tömbből
+                const blob = new Blob([byteArray], { type: 'image/png' });
+
+                // Beállítjuk a state-et a létrehozott blob objektumra
+                setImageData(blob);
+            }).catch(err => {
+                console.log(err);
+            })
+
+        };
+
+        // Adatok lekérdezése a komponens mount-olásakor
+        fetchData();
+    }, [])
+    const searchUser = (e) => {
         e.preventDefault();
+        
         const data = {
-            username: searchData
+            username: e.target.value
         }
-        dataHandler.postDataAndHandle("getUserByUsername",data)
-        .then(res=>{
-            if (res.data.length >= 1) {
-                
-            setSearchValue(res.data);
-            }else{
-                
-                setSearchValue(false);
-            }
-            
-        })
+        dataHandler.postDataAndHandle("getUserByUsername", data)
+            .then(res => {
+                if (res.data.length >= 1) {
+                    setSearchValue(res.data);
+
+
+                } else {
+
+                    setSearchValue(false);
+                }
+
+            })
     }
+
 
     return (
         <div className='shadow-2xl'>
             <div className='flex justify-end items-center py-8 w-full bg-gray h-[5vh] '>
-                <form className='mx-2' onSubmit={(e)=>searchUser(e)}>
-                    <input className='min-w-[100px] max-w-[130px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' onChange={(e)=>{
-                    if (e.target.value.length >= 2) {
-                        setSearchData(e.target.value); searchUser(e)
-                    }else{
-                        setSearchValue(false);
-                    }
-                    
-                    }} type="text" placeholder='Search Blogger'/>{searchValue !== false && <div className="absolute  min-h-[50px] bg-white rounded-md shadow-2xl w-auto grid grid-cols-1 ">
-                
-                    {searchValue.map((item,index)=>{
-                        return (<div className='flex justify-center items-center '><div className='flex justify-between items-center hover:bg-white hover:text-black text-white bg-black min-w-[200px] px-3 py-1 max-w-[400px]'>
-                             {item.profilePicture === null? <img src={account} />:<img src={item.profilePicture}/>}
-                             <div className=''>{item.username}</div>
-                        </div></div>)
-                    })}
-    
-                    </div>}</form>
-                
-                {isLogin &&<a href='' className='text-white px-4 py-2 justify-center  hover:bg-black hover:rounded-lg'><img src={bell} alt="" className='h-[5vh] shadow-2xl' /></a>}
-                {!isLogin &&<a href='/sign' className='text-white px-4 py-2 justify-center  hover:bg-black hover:rounded-lg me-3 md:me-10'><img src={login} alt="" className='h-[5vh] shadow-2xl' /></a>}
-                {isLogin &&<a href='' className='text-white px-4 py-2 justify-center  hover:bg-black hover:rounded-lg'><img src={account} alt="" className='h-[5vh] shadow-2xl' /></a>}
-                {isLogin &&<a href='' onClick={()=>{baseFun.logout()}} className='text-white px-4 py-2 justify-center  hover:bg-black hover:rounded-lgme-3 md:me-10'><img src={Logout} alt="" className='h-[5vh] shadow-2xl' /></a>}
+                <form className='mx-2' >
+                    <input className='min-w-[100px] max-w-[130px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                        onChange={(e) => {
+                            if (e.target.value.length >= 2) {
+                                
+                                searchUser(e)
+                            } else {
+                                setSearchValue(false);
+                            }
+
+                        }} type="text" placeholder='Search Blogger' />{searchValue !== false && <div className="absolute  min-h-[50px] bg-white rounded-md shadow-2xl w-auto grid grid-cols-1 ">
+                            
+
+                            {searchValue.map((item, index) => {
+                                return (
+                                    <div key={index} className='flex justify-center items-center'>
+                                        <div className='flex justify-between items-center hover:bg-white hover:text-black text-white bg-black min-w-[200px] px-3 py-1 max-w-[400px]'>
+                                            <img src={URL.createObjectURL(ImageData)} alt=""/>
+                                            <div className=''>{item.username}</div>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+
+
+
+
+                        </div>}</form>
+
+                {isLogin && <a href='' className='text-white px-4 py-2 justify-center  hover:bg-black hover:rounded-lg'><img src={bell} alt="" className='h-[5vh] shadow-2xl' /></a>}
+                {!isLogin && <a href='/sign' className='text-white px-4 py-2 justify-center  hover:bg-black hover:rounded-lg me-3 md:me-10'><img src={login} alt="" className='h-[5vh] shadow-2xl' /></a>}
+                {isLogin && <a href='' className='text-white px-4 py-2 justify-center  hover:bg-black hover:rounded-lg'><img src={account} alt="" className='h-[5vh] shadow-2xl' /></a>}
+                {isLogin && <a href='' onClick={() => { baseFun.logout() }} className='text-white px-4 py-2 justify-center  hover:bg-black hover:rounded-lgme-3 md:me-10'><img src={Logout} alt="" className='h-[5vh] shadow-2xl' /></a>}
             </div>
-            
+
             <div className='grid grid-cols-1 md:grid-cols-2 min-h-[15vh] w-full justify-between items-center bg-white'>
                 <div className='justify-start'><img src={logo} className=' md:h-[15vh]' alt="" /></div>
                 <div className='grid grid-cols-1 md:grid-cols-3   justify-start'>
-                 <a href='/' className='rounded-lg grid justify-center items-center  bg-red  hover:bg-red-400 my-2 md:my-0 md:mx-2 py-4'><button className='shadow-2xl text-white text-center'>Home</button></a>
-                {!isLogin &&<a href='/sign' className='rounded-lg grid justify-center items-center  bg-red  hover:bg-red-400 my-2 md:my-0 md:mx-2 py-4'><button className='shadow-2xl text-white text-center'>Sign</button></a>}
-                {isLogin &&<a href='#' className='rounded-lg grid justify-center items-center  bg-red  hover:bg-red-400 my-2 md:my-0 md:mx-2 py-4'><button className='shadow-2xl text-white text-center'>Create Post</button></a>}
-            </div>
+                    <a href='/' className='rounded-lg grid justify-center items-center  bg-red  hover:bg-red-400 my-2 md:my-0 md:mx-2 py-4'><button className='shadow-2xl text-white text-center'>Home</button></a>
+                    {!isLogin && <a href='/sign' className='rounded-lg grid justify-center items-center  bg-red  hover:bg-red-400 my-2 md:my-0 md:mx-2 py-4'><button className='shadow-2xl text-white text-center'>Sign</button></a>}
+                    {isLogin && <a href='#' className='rounded-lg grid justify-center items-center  bg-red  hover:bg-red-400 my-2 md:my-0 md:mx-2 py-4'><button className='shadow-2xl text-white text-center'>Create Post</button></a>}
+                </div>
 
             </div>
         </div>
