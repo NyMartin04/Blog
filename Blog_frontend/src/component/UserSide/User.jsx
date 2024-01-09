86258b841e34dff309786cb1f3fd5a9a7a8108f7
martinkovacs22 <79226892+martinkovacs22@users.noreply.github.com>
2024-01-09 13:53:20 +0100

    smallChange

--- Blog_frontend/src/component/UserSide/User.jsx
+++ Blog_frontend/src/component/UserSide/User.jsx
@@ -1,11 +1,27 @@
 import React ,{useEffect,useState} from "react";
 import Navbar from "../Navbar/Navbar";
-
+import baseFun from "../../config/baseFun.js";
+import dataHandler from "../../config/http.js";
 export default function User(params) {
     
     useEffect(()=>{
-       console.log(window.location.href.split("#")[1]);
+        try {
+            const UserID = {id:parseInt(window.location.href.split("#")[1])}  
+            dataHandler.postDataAndHandle("getUserByID",UserID).then(res=>{
+                if (res.data.lenght) {
+                    
+                }
+                console.log(res.data.le);
+            }).catch(err=>{
+                console.log(err);
+                baseFun.redirect("/");
+            })
+        } catch (error) {
+                console.log("nem jo");
+        } 
     })
+
+    
 
     return(<>
     <Navbar />
