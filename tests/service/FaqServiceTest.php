42639a0b325d7980387c1e8b770caf065e4ac817
Exe <125662764+2003Klink@users.noreply.github.com>
2024-01-12 10:57:28 +0100

    FaqTest comment

--- tests/service/FaqServiceTest.php
+++ tests/service/FaqServiceTest.php
@@ -6,10 +6,10 @@ require_once __DIR__ . '\..\..\Autoloader.php';
 
 class FaqServiceTest extends TestCase{
 
-    public function testcreateFaqSuccess(){
-        $dataSend = array(
-            "name"=>"test_".rand(0,10000000));
-        $classes = FaqService::createFaq($dataSend);
-        $this->assertFalse($classes["err"]);
-    }
+    // public function testcreateFaqSuccess(){
+    //     $dataSend = array(
+    //         "name"=>"test_".rand(0,10000000));
+    //     $classes = FaqService::createFaq($dataSend);
+    //     $this->assertFalse($classes["err"]);
+    // }
 }
